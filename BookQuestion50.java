e037e4a845b9b97dd7810b26b214ed983ca6da82
vincentbenesen <60351647+vincentbenesen@users.noreply.github.com>
2020-03-13 16:27:11 -0400

    Add files via upload

---
+++ BookQuestion50.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BookQuestion50{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Write the temperature today > ");
+		int temperature = sc.nextInt();
+
+		if (temperature > 110 || temperature < -5) {
+			System.out.println("The temperature entered is outside the valid range");
+		} else if (temperature >= 90) {
+			System.out.println("It is summer");
+		} else if (temperature >= 70 && temperature < 90) {
+			System.out.println("It is spring ");
+		} else if (temperature >= 50 && temperature < 70) {
+			System.out.println("It is fall");
+		} else if (temperature < 50) {
+			System.out.println("It is winter");
+		} else if (temperature > 110 || temperature < -5) {
+			System.out.println("The temperature entered is outside the valid range");
+		}
+	}
+}
